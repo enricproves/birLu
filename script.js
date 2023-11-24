89c243e0d4792510379cb04474705292c81f5157
enricproves <151690782+enricproves@users.noreply.github.com>
2023-11-24 20:32:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -32,6 +32,11 @@ document.addEventListener('DOMContentLoaded', function() {
     const fruitImg = new Image();
     fruitImg.src = 'whiteChipCookie.png'; // Path to fruit image
 
+    // Prevent default touchmove behavior to avoid scrolling on the canvas
+    snakeCanvas.addEventListener('touchmove', function(event) {
+        event.preventDefault();
+    });
+
 
     startSnakeButton.addEventListener('click', function() {
         initialScreen.style.display = 'none'; // Hide initial screen
